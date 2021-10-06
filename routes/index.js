00382684d1d9898a7cea5ab11e3c0e599f13c8d4
Henrique Jacquier <henriquejacquier@gmail.com>
2021-10-06 15:40:12 -0300

    initial form idea

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {title: "Seu FazTudo"});
+});
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 module.exports = router;
